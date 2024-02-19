f02e8e910c97c6f18eb6c9764518b2a83de554b0
ElTinmar <martin.privat@hotmail.fr>
2024-02-19 15:43:20 +0100

    update

--- scrape.py
+++ scrape.py
@@ -1,8 +1,21 @@
-url = {
+# download data and save as csv
+
+import requests
+
+data = 'Data'
+urls = {
   'cpu': ['https://www.cpubenchmark.net/CPU_mega_page.html'],
   'gpu': ['https://www.videocardbenchmark.net/GPU_mega_page.html'],
-  'hdd': ['https://www.harddrivebenchmark.net/hdd-mega-page.html]'
+  'hdd': ['https://www.harddrivebenchmark.net/hdd-mega-page.html'],
   'ram': ['https://www.memorybenchmark.net/ram_list.php',  
           'https://www.memorybenchmark.net/ram_list-ddr4.php',  
           'https://www.memorybenchmark.net/ram_list-ddr3.php']
 }
+
+for device, address_list in urls.items():
+  for address  in address_list:
+    res = requests.get(address)
+    if res.status_code != 200:
+      raise requests.ConnectionError(address)
+
+    
