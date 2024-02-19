2bd0ca0a65b10cfe3427dce9ac70ae1d15d9d698
Tinmar <ElTinmar@users.noreply.github.com>
2024-02-19 15:28:07 +0100

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,8 @@
+url = {
+  'cpu': ['https://www.cpubenchmark.net/CPU_mega_page.html'],
+  'gpu': ['https://www.videocardbenchmark.net/GPU_mega_page.html'],
+  'hdd': ['https://www.harddrivebenchmark.net/hdd-mega-page.html]'
+  'ram': ['https://www.memorybenchmark.net/ram_list.php',  
+          'https://www.memorybenchmark.net/ram_list-ddr4.php',  
+          'https://www.memorybenchmark.net/ram_list-ddr3.php']
+}
